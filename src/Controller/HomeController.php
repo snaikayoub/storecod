e7ee95623f272d47e7d646e22ffc0392992279d6
snaikayoub <snaikayoub@gmail.com>
2026-04-21 20:06:37 +0100

    Make Meta conversion value dynamic on thank you page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -27,9 +29,25 @@ class HomeController extends AbstractController
         ]);
     }
 
-    #[Route('/merci', name: 'thank_you')]
-    public function thankYou(): Response
+    #[Route('/merci/{id}', name: 'thank_you', requirements: ['id' => '\\d+'])]
+    public function thankYou(int $id, Request $request, EntityManagerInterface $em): Response
     {
-        return $this->render('thank_you.html.twig');
+        /** @var Order|null $order */
+        $order = $em->getRepository(Order::class)->find($id);
+        if (!$order) {
+            throw $this->createNotFoundException('Commande introuvable.');
+        }
+
+        $trackOrderId = (int) $request->getSession()->get('thank_you_track_order_id', 0);
+        $shouldTrack = $trackOrderId === $order->getId();
+
+        if ($shouldTrack) {
+            $request->getSession()->remove('thank_you_track_order_id');
+        }
+
+        return $this->render('thank_you.html.twig', [
+            'order' => $order,
+            'shouldTrackLead' => $shouldTrack,
+        ]);
     }
 }
